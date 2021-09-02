60469c263425e03644076e224be8b9886c8ed619
Rohina Naaz <rohinanaaz@gmail.com>
2021-09-02 11:02:03 +0530

    Update Main.java

--- src/com/greatlearning/OOPs/department/Main.java
+++ src/com/greatlearning/OOPs/department/Main.java
@@ -7,22 +7,29 @@ public class Main {
 		AdminDepartment admin = new AdminDepartment();
 		HrDepartment hr = new HrDepartment();
 		TechDepartment tech = new TechDepartment();
+		
+		SuperDepartment super_admin = new AdminDepartment();
+		SuperDepartment super_hr = new HrDepartment();
+		SuperDepartment super_tech = new TechDepartment();
+		
 		System.out.println("Welcome to "+admin.departmentName());
 		System.out.println(admin.getTodaysWork());
 		System.out.println((admin.getWorkDeadline()));
-		System.out.println(admin.isTodayAHoliday());
+		System.out.println(super_admin.isTodayAHoliday());
 		System.out.println();
+		
 		System.out.println("Welcome to "+hr.departmentName());
 		System.out.println(hr.doActivity());
 		System.out.println(hr.getTodaysWork());
 		System.out.println((hr.getWorkDeadline()));
-		System.out.println(hr.isTodayAHoliday());
+		System.out.println(super_hr.isTodayAHoliday());
 		System.out.println();
+		
 		System.out.println("Welcome to "+tech.departmentName());
 		System.out.println(tech.getTodaysWork());
 		System.out.println((tech.getWorkDeadline()));
 		System.out.println(tech.getTechStackInformation());
-		System.out.println(tech.isTodayAHoliday());
+		System.out.println(super_tech.isTodayAHoliday());
 	}
 
 }
